3f5bfaf64a777fa2d53864df4eb6d09af6e2cd87
novac0re-i3 <novafarazi33@gmail.com>
2026-02-05 23:25:25 +0600

    added general classes

---
+++ basic.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+#always do these before using github
+
+
+#1__ git init ____ means intialize git
+#2__ git add (any file name) or .  ____ dot means every file in the folder
+#3__ git commit -m "any message goes here"  ____ -m means -message. commit is used to put the updated things of the code into message chaning the file into different versions making it easier for programmers to keep track of changes
+#4__ git branch -M main ____ means to connect the main page of github(main branch)
+#5__ git remote add origin https://github.com/novac0re-i3/Student-Mnagement-System.git  _____ means to add the remote work we do in an editor(Ex: VScode) to github i.e. to send it there
+#6__ git push -u origin main ____ means to push all the done work to github
+
+
+#always start a new terminal for using any command related to git
+#after pushing the main file in github then we can add any commit(changes) into the git by using command 2,3,6 (add, commit, push)(better to use add .)
---
+++ main.py
@@ -0,0 +1,38 @@
+#Rupnagar committee Family management system
+class families:
+    def __init__(self,family_name,members):
+        self.family_name =family_name
+        self.members=members
+        
+    def show_info(self):
+        print(f"Family Name:{self.family_name} , Number of Members: {self.members}")
+        
+class parents(families):
+    def __init__(self,family_name,members, dad_name, mom_name, total_income=0):
+        super().__init__(family_name,members)
+        self.dad_name=dad_name
+        self.mom_name=mom_name
+        self.__total_income=total_income  #Private
+        
+    def set_total_income(self,total_income):
+        self.__total_income=total_income
+
+    def get_total_income(self):
+        return self.__total_income
+    
+    def show_info(self):
+        super().show_info()
+        print(f"Father's name: {self.dad_name} , Mother's name: {self.mom_name} , Total Income : {self.__total_income} ")
+        
+    def __del__(self):
+        print(f"The {self.family_name} family has been removed from the system")
+        
+class person(families):
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def show_info(self):
+        super().show_info()
+        print(f"Admin info--- name:{self.name} , age: {self.age}")
+        
